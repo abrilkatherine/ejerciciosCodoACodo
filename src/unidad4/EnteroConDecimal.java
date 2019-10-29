537875b31d29512e67e4af78338db988db652dab
Abril <abrilkatherine@hotmail.com>
2019-10-28 21:56:00 -0300

    Primeros ejercicios

---
+++ src/unidad4/EnteroConDecimal.java
@@ -0,0 +1,25 @@
+package unidad4;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class EnteroConDecimal {
+    public static void main(String[] args) {
+
+        /*  3)  Desarrollar un algoritmo que permita ingresar dos números enteros por teclado. La computadora deberá
+            mostrar su cociente con decimales. Recuerde utilizar Casting para obtener el resultado con los decimales
+            correspondientes. */
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número:");
+        int num1 = entrada.nextInt();
+
+        System.out.println("Ingrese el segundo número:");
+        int num2 = entrada.nextInt();
+
+        Double cociente = (double) num1 / num2;
+
+        System.out.printf("Cociente: %.2f", cociente);
+    }
+}
--- src/unidad4/NumeroEntero.java
+++ src/unidad4/NumeroEntero.java
@@ -2,7 +2,7 @@ package unidad4;
 
 import java.util.Scanner;
 
-public class NumerosEnteros {
+public class NumeroEntero {
     public static void main(String[] args){
 
         /*2) Desarrollar un algoritmo que permita ingresar dos números enteros por
@@ -11,13 +11,16 @@ public class NumerosEnteros {
 
         Scanner entrada = new Scanner(System.in);
 
-        int numero1;
-        int numero2;
+        System.out.println("Ingresar un numero");
+        int numero1 = entrada.nextInt();
 
-        System.out.println("Ingresar numeros enteros");
+        System.out.println("Ingresar otro numero");
+        int numero2 = entrada.nextInt();
 
-        numero1=entrada.nextInt();
-        numero2=entrada.nextInt();
+        int cociente = numero1 / numero2;
+        int resto= numero1 % numero2;
 
+        System.out.println("cociente: " + cociente);
+        System.out.println("Resto: " + resto);
     }
 }
